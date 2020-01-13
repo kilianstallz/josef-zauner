7905881658ea59d1d17248301a2d63e17a7af0c9
kilianstallz <kp.stallinger@gmail.com>
2020-01-13 16:54:47 +0100

    init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <section class="main-bg flex flex-col">
-      <header class="flex row p-10">
+      <img
+        src="~/assets/bg-white.svg"
+        alt="BG"
+        class="absolute z-0 h-screen bg-image"
+      />
+      <header class="flex row p-10 z-50">
         <div class="logo flex pr-24">
           <img src="~/assets/Logo.svg" alt="Logo" class="pr-4" />
           <div style="font-size: 14px;" class="flex font-semibold flex-col">
@@ -10,13 +15,13 @@
           </div>
         </div>
         <nav class="font-semibold">
-          <span class="pr-12">Leistungen</span>
-          <span class="pr-12">Über Mich</span>
-          <span class="pr-12">Kontakt</span>
-          <span>Blog</span>
+          <nuxt-link to="/" class="cursor-pointer pr-12">Leistungen</nuxt-link>
+          <nuxt-link to="/" class="cursor-pointer pr-12">Über Mich</nuxt-link>
+          <nuxt-link to="/" class="cursor-pointer pr-12">Kontakt</nuxt-link>
+          <nuxt-link to="/" class="cursor-pointer">Blog</nuxt-link>
         </nav>
       </header>
-      <div class="intro flex flex-col">
+      <div class="intro flex flex-col z-50">
         <span class="intro-head"
           >Consulting <br />
           Gutachten <br />
@@ -36,9 +41,76 @@
         class="absolute cert-image"
       />
     </section>
-    <nuxt />
+    <section class="flex flex-col pt-24">
+      <h2 style="font-size: 42px;" class="text-center">
+        Gemeinsam Maßnahmen ergreifen
+      </h2>
+      <h2 style="font-size: 40px;" class="text-center">
+        Chancen am Markt warnehmen
+      </h2>
+      <!-- Leistungen Rows -->
+      <div class="leistungen">
+        <!-- 1 -->
+        <div class="leistungen_row flex row">
+          <div class="leistungen_text">
+            <h3>Consulting</h3>
+            <p>
+              Ich unterstütze Sie beim Fortschritt in der Unternehmens-,
+              Produktions- und Einkaufsorganisation.
+            </p>
+            <nuxt-link to="/consulting">
+              Mehr erfahren ->
+            </nuxt-link>
+          </div>
+          <img
+            src="~/assets/undraw_quiz_nlyh.svg"
+            alt="Quiz"
+            class="leistungen_img"
+          />
+        </div>
+        <!-- 2 -->
+        <div class="leistungen_row flex row">
+          <div class="leistungen_text">
+            <h3>Gutachten</h3>
+            <p>
+              Ich bin allgemein beeideter und gerichtlich zertifizierter
+              Sachverständiger. Ich erstelle Privat- und Gerichtsgutachten.
+            </p>
+            <nuxt-link to="/gutachten">
+              Mehr erfahren ->
+            </nuxt-link>
+          </div>
+          <img
+            src="~/assets/undraw_confirmation_2uy0.svg"
+            alt="Gutachten"
+            class="leistungen_img"
+          />
+        </div>
+        <!-- 3 -->
+        <div class="leistungen_row flex row">
+          <div class="leistungen_text">
+            <h3>Projektbegleitung</h3>
+            <p>
+              Ich unterstütze den Metallbauer bei der Entwicklung rationeller
+              Detaillösungen.
+            </p>
+            <nuxt-link to="/begleitung">
+              Mehr erfahren ->
+            </nuxt-link>
+          </div>
+          <img src="~/assets/folder.svg" alt="Folder" class="leistungen_img" />
+        </div>
+      </div>
+    </section>
+    <!-- <nuxt /> -->
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
 
 <style>
 html {
@@ -95,14 +167,12 @@ html {
 
 .cert-image {
   right: 44px;
-  top: 120px;
+  top: 150px;
+  width: 50%;
 }
 
 .main-bg {
   background: #9bfaf5;
-  background-image: url('~assets/bg-white.svg');
-  background-repeat: no-repeat;
-  background-size: 100vh;
   height: 100vh;
   width: 100%;
 }
@@ -131,5 +201,43 @@ html {
 .cta > span {
   font-weight: 500;
   color: #333;
+}
+
+/**
+* Leistungen
+ */
+
+.leistungen {
+  max-width: 890px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.leistungen_row {
+  margin-top: 120px;
+  display: flex;
+  justify-content: space-between;
+}
+.leistungen_row:nth-child(2n) {
+  flex-direction: row-reverse;
+}
+
+.leistungen_text h3 {
+  font-size: 40px;
+  font-weight: 500;
+  margin-bottom: 16px;
+}
+.leistungen_text p {
+  font-size: 18px;
+  margin-bottom: 26px;
+  max-width: 360px;
+}
+.leistungen_text a {
+  color: #2979ff;
+}
+
+.leistungen_img {
+  width: 40%;
 }
 </style>
